3da2c41294ed3256eb55681a383ea3a488603b2c
Katha Gualdrón <gualdronmarquezw2008@gmail.com>
2026-04-30 17:13:04 -0500

    segui actualizando mi informacion de models usuarios_agro

---
+++ models/rol.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Rol struct {
+	IdRol             int        `json:"id_rol"`
+	NombreRol         string     `json:"nombre_rol"`
+	Descripcion       *string    `json:"descripcion"`
+	Activo            bool       `json:"activo"`
+	FechaCreacion     time.Time  `json:"fecha_creacion"`
+	FechaModificacion time.Time  `json:"fecha_modificacion"`
+}
