7ed92effc083f49218af37096a62e00738490cb8
Katha Gualdrón <gualdronmarquezw2008@gmail.com>
2026-05-04 14:34:36 -0500

    ya termine de actualizar la informacion y cambie el puerto

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	router := mux.NewRouter()
 	routes.RegistrarRutas(router)
 
-	log.Println("Servidor USUARIOS escuchando en :8083")
-	log.Fatal(http.ListenAndServe(":8083", router))
+	log.Println("Servidor USUARIOS escuchando en :8081")
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
+ 
